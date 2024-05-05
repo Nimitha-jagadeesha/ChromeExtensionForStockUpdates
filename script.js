ff8ef7c3ecc9f087e26e6b0c51eabe2a964a966c
Nimitha J <58389915+Nimitha-jagadeesha@users.noreply.github.com>
2024-05-05 17:47:43 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-fetch('https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=JPM&interval=5min&apikey=<key>')
+fetch('https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=JPM&interval=5min&apikey=L1YMGQ2TVW2QLHH4')
     .then(data => data.json())
     .then(htmlData =>{
         const data = htmlData['Time Series (5min)'][htmlData['Meta Data']['3. Last Refreshed']];
@@ -13,7 +13,7 @@ fetch('https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=JP
         lowElement.innerHTML = low;
     })
 
-    fetch('https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=AMZN&interval=5min&apikey=<key>')
+    fetch('https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=AMZN&interval=5min&apikey=FIKW3LZYGQK97C9K')
     .then(data => data.json())
     .then(htmlData =>{
         const data = htmlData['Time Series (5min)'][htmlData['Meta Data']['3. Last Refreshed']];
@@ -27,6 +27,23 @@ fetch('https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=JP
         const lowElement = document.getElementById("amzlow");
         lowElement.innerHTML = low;
     })
+
+
+    fetch('https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=SAP&interval=5min&apikey=FIKW3LZYGQK97C9K')
+    .then(data => data.json())
+    .then(htmlData =>{
+        const data = htmlData['Time Series (5min)'][htmlData['Meta Data']['3. Last Refreshed']];
+        const open = data['1. open']
+        const high = data['2. high'] 
+        const low = data['3. low']
+        const openElement = document.getElementById("sapopen");
+        openElement.innerHTML = open;
+        const highElement = document.getElementById("saphigh");
+        highElement.innerHTML = high;
+        const lowElement = document.getElementById("saplow");
+        lowElement.innerHTML = low;
+    })
+
 
 
  
